4236cacaf735a98ad61af3d0d8558a5b952afb36
Mateus Zaparoli <mateuszaparoli7@gmail.com>
2025-01-09 15:35:53 -0300

    Tratamento das palavras

--- simulador.py
+++ simulador.py
@@ -15,12 +15,32 @@ def main():
     #leitura do arquivo de entrada
     arquivoEntrada = sys.argv[4]
     with open(arquivoEntrada, 'r') as file:
-        palavras = file.readlines()
+        palavras = [int(linha.strip(), 16) for linha in file]
 
     # calculando quantas linhas temos e offsets
     numLinhas = tamCache // tamLinha
     offsetPalavra = log2(tamLinha)
     numGrupos = numLinhas // tamGrupo
+    offsetGrupo = log2(tamGrupo)
+
+    # obter as palavras de entrada em binário
+    palavrasBin = [bin(palavra)[2:].zfill(32) for palavra in palavras]
+    print(palavrasBin)
+    
+    #retirando o offset de palavra e de grupo
+    #TEMOS QUE ANALISAR SE PRECISAMOS GUARDAR OS OFFSETS DE GRUPO
+    #PRA FAZER A CONTA DE QUAL GRUPO COLOCAR A PALAVRA
+    palavrasBin = [palavra[:-int(offsetPalavra + offsetGrupo)].zfill(32) for palavra in palavrasBin]
+    print(palavrasBin)
+
+    #obtendo os identificadores
+    addrs = [f"0x{hex(int(palavra, 2))[2:].zfill(8).upper()}" for palavra in palavrasBin]
+    print(addrs)
+
+    #vetor do bit de validade para cada linha
+    validades = [0] * numLinhas
+
+    
 
     #prints de debug
     print(offsetPalavra)
@@ -32,7 +52,7 @@ def main():
     for j in range(len(palavras)):
         printCabeçalho()
         for i in range(numLinhas):
-            print(f"{i:03d}")
+            print(f"{i:03d} {validades[i]}")
 
 if __name__ == "__main__":
     main()
