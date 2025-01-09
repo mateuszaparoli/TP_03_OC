3530964655f4e8d33bbd1aa9f9ee7755ece7a88c
Mateus Zaparoli <mateuszaparoli7@gmail.com>
2025-01-09 01:40:47 -0300

    mudanças nomes

--- simulador.py
+++ simulador.py
@@ -2,13 +2,13 @@ import sys
 
 def main():
     
-    tamanhoCache = int(sys.argv[1]) # quantas bytes na cache
-    tamanhoLinha = int(sys.argv[2]) # quantos bytes por linha
-    tamanhoGrupo = int(sys.argv[3]) # quantas linhas por grupo
+    tamCache = int(sys.argv[1]) # quantas bytes na cache
+    tamLinha = int(sys.argv[2]) # quantos bytes por linha
+    tamGrupo = int(sys.argv[3]) # quantas linhas por grupo
     arquivoEntrada = sys.argv[4]
 
     # calculando quantas linhas temos
-    linhas = tamanhoCache // tamanhoLinha
+    numLinhas = tamCache // tamLinha
 
 
 if __name__ == "__main__":
