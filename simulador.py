799af30e9a0a0bf660124850ac7051e1738fc44a
Mateus Zaparoli <mateuszaparoli7@gmail.com>
2025-01-09 13:24:53 -0300

    prints e tratamentos de entrada

--- simulador.py
+++ simulador.py
@@ -7,19 +7,32 @@ def printCabeçalho():
 
 def main():
     
+
+    #leitura dos argumentos
     tamCache = int(sys.argv[1]) # quantas bytes na cache
     tamLinha = int(sys.argv[2]) # quantos bytes por linha
     tamGrupo = int(sys.argv[3]) # quantas linhas por grupo
+    #leitura do arquivo de entrada
     arquivoEntrada = sys.argv[4]
+    with open(arquivoEntrada, 'r') as file:
+        palavras = file.readlines()
 
-    # calculando quantas linhas temos
+    # calculando quantas linhas temos e offsets
     numLinhas = tamCache // tamLinha
-
     offsetPalavra = log2(tamLinha)
+    numGrupos = numLinhas // tamGrupo
 
-    printCabeçalho()
+    #prints de debug
+    print(offsetPalavra)
+    print(numLinhas)
+    for i in range(len(palavras)):
+        print(palavras[i])
 
-    
+    # prints resultados
+    for j in range(len(palavras)):
+        printCabeçalho()
+        for i in range(numLinhas):
+            print(f"{i:03d}")
 
 if __name__ == "__main__":
     main()
